d80bf2d2af70b8c84fd165041c66e8a9ad3a2b20
Jay Nahata <nahatajay6@gmail.com>
2024-09-14 01:02:44 +0530

    push

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///legalease.db'  # Using SQLite for local dev
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
